350955af8066b5a170fe252baf0197499e45002d
Xianjing LAI <xlaieva@163.com>
2025-03-28 10:22:29 +1100

    login + register +some popup html

--- frontend/src/main.js
+++
@@ -1,5 +0,0 @@
-import { BACKEND_PORT } from './config.js';
-// A helper you may want to use when uploading new images to the server.
-import { fileToDataUrl } from './helpers.js';
-
-console.log('Let\'s go!');
